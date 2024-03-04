fa1c6b3e70db0236e4ab83319af8dedef98f6f38
Serge Sinkevych <serge@avicenaheart.com>
2024-03-03 21:02:31 -0800

    first commit

---
+++ SwiftUIWorkshop/SwiftUIWorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWorkshopApp.swift
+//  SwiftUIWorkshop
+//
+//  Created by Serge Sinkevych on 3/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
